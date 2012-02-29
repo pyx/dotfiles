277e8ed384eab61a19bdacf06ff33ac1ae33994f
Philip Xu <pyx@xrefactor.com>
2012-02-29 06:24:04 -0500

    added rc template for getmail

---
+++ config/getmail/do_setup_getmail_rc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -d "$HOME/.getmail/" ]; then
+  echo "you have $HOME/.getmail set up already, nothing to be done."
+  exit 0
+else
+  mkdir -m 0700 $HOME/.getmail &&
+  cp getnailrc $HOME/.getmail/ &&
+  chmod 0600 $HOME/.getmail/getnailrc &&
+  echo "copied getmail rc file template into $HOME/.getmail/, change the file as needed."
+fi
