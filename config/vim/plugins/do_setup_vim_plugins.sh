f627e55ce3719f5a5e13b3c37c2392f5388b019d
Philip Xu <pyx@xrefactor.com>
2012-08-07 19:17:04 -0400

    added srcipt to setup vim plugins

---
+++ config/vim/plugins/do_setup_vim_plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir -p $HOME/.vim/bundle &&
+cd $HOME/.vim/bundle &&
+rm -rf *
+grep -E '^" (hg|git|svn)' $HOME/.vimrc | cut --bytes=3- | sh &&
+# assuming pathogen was installed. otherwise(vundle, maybe) we do't need this script.
+mkdir -p $HOME/.vim/autoload &&
+rm -f $HOME/.vim/autoload/pathogen.vim &&
+ln -sn $HOME/.vim/bundle/vim-pathogen/autoload/pathogen.vim $HOME/.vim/autoload/pathogen.vim
