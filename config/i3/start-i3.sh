85fac6c343371898b031d0c246b820ddb5daf11d
Philip Xu <pyx@xrefactor.com>
2012-11-25 20:05:15 -0500

    added configuration for i3.

---
+++ config/i3/start-i3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+nm-applet &
+# -or-
+#wicd-gtk --tray  &
+ibus-daemon -d -x -r
+xcompmgr -c &
+conky &
+/usr/bin/volumeicon &
+xsetroot -cursor_name left_ptr
+# Caps Lock as Control, clear with: setxkbmap -option ''
+setxkbmap -option ctrl:nocaps
+# make this a symlink to your favorite wallpaper
+fbsetbg ~/.background
