6ef35f2ed3fce1e007c672cc10b3e064aa37f7b1
Philip Xu <pyx@xrefactor.com>
2013-01-13 03:08:33 -0500

    fixed typo.

--- config/vim/plugins/do_setup_vim_plugins.sh
+++ config/vim/plugins/do_setup_vim_plugins.sh
@@ -3,7 +3,7 @@ mkdir -p "$HOME"/.vim/bundle &&
 cd "$HOME"/.vim/bundle &&
 rm -rf */ &&
 grep -E '^" (hg|git|svn)' "$HOME"/.vimrc | cut --bytes=3- | sh &&
-# assuming pathogen was installed. otherwise(vundle, maybe) we do't need this script.
+# assuming pathogen was installed. otherwise(vundle, maybe) we don't need this script.
 mkdir -p "$HOME"/.vim/autoload &&
 rm -f "$HOME"/.vim/autoload/pathogen.vim &&
 ln -sn "$HOME"/.vim/bundle/vim-pathogen/autoload/pathogen.vim "$HOME"/.vim/autoload/pathogen.vim
