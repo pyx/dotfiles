a3d1f8d0189a3dd7655e9d4082a31940be92e152
Philip Xu <pyx@xrefactor.com>
2014-03-07 23:43:03 -0500

    Moved syntax files into bundle misc

--- config/vim/syntax/do_symlink_vala.vim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p "$HOME"/.vim/syntax &&
-cd "$HOME"/.vim/syntax/ &&
-rm -f vala.vim &&
-ln -sn "$OLDPWD"/vala.vim
--- config/vim/syntax/no_symlink_jinja2.vim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p "$HOME"/.vim/syntax &&
-cd "$HOME"/.vim/syntax/ &&
-rm -f jinja2.vim &&
-ln -sn "$OLDPWD"/jinja2.vim
