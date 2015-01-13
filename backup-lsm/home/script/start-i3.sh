006c61598add37ed7d6bcdbbce7019cb2d734958
Philip Xu <pyx@xrefactor.com>
2015-01-12 19:58:07 -0500

    Imported configuration files

---
+++ backup-lsm/home/script/start-i3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#nm-applet &
+# -or-
+dunst &
+wicd-gtk --tray &
+ibus-daemon -d -x -r
+compton -c &
+conky &
+/usr/bin/volumeicon &
+xsetroot -cursor_name left_ptr
+# Caps Lock as Control, clear with: setxkbmap -option ''
+setxkbmap -option ctrl:nocaps
+# make this a symlink to your favorite wallpaper
+feh --bg-fill ~/.background.0 ~/.background.1
