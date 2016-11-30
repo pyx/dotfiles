714a5904ba212415c52ab36dacb87208b010945e
Philip Xu <pyx@xrefactor.com>
2016-11-30 04:12:24 -0500

    Forced using system command, or tab-completion will fail

--- config/fish/functions/hg_file_path_relative_to_cwd.fish
+++ config/fish/functions/hg_file_path_relative_to_cwd.fish
@@ -5,6 +5,6 @@ function hg_file_path_relative_to_cwd --description 'print file path in hg repos
 	set -l cwd (realpath .)
 	set -l repo_root (realpath (hg root ^/dev/null) ^/dev/null)
 	for filename in $argv
-		realpath --relative-to=$cwd $repo_root/$filename
+		/usr/bin/realpath --relative-to=$cwd $repo_root/$filename
 	end ^/dev/null
 end
