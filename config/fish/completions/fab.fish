fbe5e9688539ec6e021de1cb1b1e1825b77f5318
Philip Xu <pyx@xrefactor.com>
2024-03-28 03:28:25 -0400

    Updated config

--- config/fish/completions/fab.fish
+++
@@ -1,3 +0,0 @@
-# Completion for fabric
-
-complete -c fab --description 'Task in fabfile' -a '(fab --shortlist 2>/dev/null)' -x
--- config/fish/do_symlink_completions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p "$HOME"/.config/fish/ &&
-cd "$HOME"/.config/fish/ &&
-# assuming this is a symlink
-rm -f completions &&
-ln -sn "$OLDPWD"/completions
